6937f89e716b8e9624f52e1a55ab532ff30340bf
HanOse <hannah@softwire.com>
2025-07-22 16:53:43 +0100

    Added router, a history page, added it as a route in main.tsx and tested page in browser

---
+++ src/history.tsx
@@ -0,0 +1,10 @@
+function History() {
+  return (
+    <div>
+        <h1>History Page</h1>
+        <p>This page will display the history of bus stops and arrivals.</p>
+    </div>
+  );
+}
+
+export default History;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import './styles/index.css'
 import App from './App.tsx'
+import History from './history.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
