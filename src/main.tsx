8b9156845a64f748a6e91f9d14265fb4628d1485
HanOse <hannah@softwire.com>
2025-07-22 16:55:11 +0100

    Created a folder called pages in src and added history.tsx to it

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import './styles/index.css'
 import App from './App.tsx'
-import History from './history.tsx'
+import History from './pages/history.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
